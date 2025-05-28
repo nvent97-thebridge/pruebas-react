c273fb8603ac69fe910b848bd18237b282ee7b57
xavi <antunez.xavi.grao@gmail.com>
2025-05-28 13:52:50 +0200

    ej useEffect resuelto

--- src/components/InputValidation.jsx
+++ src/components/InputValidation.jsx
@@ -1,11 +1,32 @@
+import { useEffect, useState } from "react";
+
 const InputValidation = () => {
+  const [password, setPassword] = useState("");
+  const [validationMessage, setValidationMessage] = useState("");
+
   const handleInputChange = (event) => {
-    console.log("Contrasenia: ", event.target.value);
+    setPassword(event.target.value);
   };
+
+  useEffect(() => {
+    if (!password) {
+      return;
+    }
+    if (password.length > 5) {
+      setValidationMessage("Valid password");
+    } else if (password.length < 6) {
+      setValidationMessage("Invalid password");
+    }
+  }, [password]);
   // Si la contrasenia tiene menos de 5 caracteres
   // mostrar texto de "Invalid pass"
   // Si tiene mas de 5 mostrar "Valid pass-"
-  return <input onChange={handleInputChange} placeholder="Ingrese password" />;
+  return (
+    <>
+      <input onChange={handleInputChange} placeholder="Ingrese password" />
+      {validationMessage}
+    </>
+  );
 };
 
 export default InputValidation;
